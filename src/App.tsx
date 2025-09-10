95a76d33a3287dad56ddc9b2312d08294dfcf834
mmista <milosz.mista@gmail.com>
2025-09-10 20:23:59 +0200

    Centralize header state management

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { SOATable } from './components/SOATable';
 import { SOALegend } from './components/SOALegend';
 import { SOAData } from './types/soa';
+import { useTimelineHeaderManagement } from './hooks/useTimelineHeaderManagement';
 
 const initialData: SOAData = {
   periods: [
@@ -159,12 +160,16 @@ const initialData: SOAData = {
 
 function App() {
   const [soaData, setSoaData] = useState<SOAData>(initialData);
+  
+  // Centralized header management
+  const headerManagement = useTimelineHeaderManagement();
 
   return (
     <div className="flex flex-col h-screen bg-gray-50">
       <SOATable 
         data={soaData} 
         onDataChange={setSoaData}
+        headerManagement={headerManagement}
       />
       <SOALegend />
     </div>
