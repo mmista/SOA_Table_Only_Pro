ed9d61b4f4a7856bf1a673b3f2ef5e2b689671af
mmista <milosz.mista@gmail.com>
2025-09-11 11:09:57 +0200

    Fix App.tsx default export error

--- src/App.tsx
+++ src/App.tsx
@@ -1,2 +1,26 @@
-const initialData: SOAData = generateSampleData();
-// Generate initial sample data
+import React from 'react';
+import { SOATable } from './components/SOATable';
+import { useTimelineHeaderManagement } from './hooks/useTimelineHeaderManagement';
+import { generateSampleData } from './utils/presetData';
+import { SOAData } from './types/soa';
+
+function App() {
+  const [soaData, setSoaData] = React.useState<SOAData>(() => generateSampleData());
+  const headerManagement = useTimelineHeaderManagement();
+
+  const handleDataChange = (newData: SOAData) => {
+    setSoaData(newData);
+  };
+
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <SOATable
+        data={soaData}
+        onDataChange={handleDataChange}
+        headerManagement={headerManagement}
+      />
+    </div>
+  );
+}
+
+export default App;
